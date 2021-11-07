822555327582828c740ead1197acec327957932f
Markus Paasovaara <markus.paasovaara@gmail.com>
2021-11-07 12:30:55 +0200

    Get data from Harvest

---
+++ integration/README.md
@@ -0,0 +1,12 @@
+# Harvest integration
+
+This will fetch hours from Harvest for each named person, applies some tranformations and stores the output. 
+
+## Secrets
+These environment variables need to be set:
+
+```
+export HARVEST_ACCOUNT_ID=...
+export HARVEST_SECRET=...
+```  
+
--- integration/src/harvest.ts
+++ integration/src/harvest.ts
@@ -1,8 +1,29 @@
-const world = 'world';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { processenv } from 'processenv';
 
-export function hello(to: string = world): string {
-	return `Hello ${to}!`;
+const config: AxiosRequestConfig = {
+	baseURL: 'https://api.harvestapp.com/api/v2/',
+	headers: {
+		'Content-Type': 'application/json',
+		'Harvest-Account-ID': `${processenv('HARVEST_ACCOUNT_ID')}`,
+		'User-Agent': 'Harvest-Mepco transformer',
+		'Authorization': `Bearer ${processenv('HARVEST_SECRET')}`,
+	}
+};
+const client: AxiosInstance = axios.create(config);
+
+interface User {
+	firstName: string;
+	lastName: string;
+	id: number;
 }
 
-console.log(hello());
-console.log(hello('mama'));
+function findUser(name: string): void {
+	client.get('/users/me.json') // TODO Fix correct endpoint 
+		.then((response) => {
+			console.log(response.status);
+			console.log(response.data);
+		});
+}
+
+findUser('P');
