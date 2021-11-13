3f61cfd3d121787ab52dca91d5c7cd3f4e65f990
Markus Paasovaara <markus.paasovaara@gmail.com>
2021-11-13 09:05:39 +0200

    Refactored client to separate file

--- integration/src/harvest.ts
+++ integration/src/harvest.ts
@@ -1,34 +1,5 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
-import { processenv } from 'processenv';
-import { AxiosPromise } from 'axios';
-import { request } from 'http';
-
-const config: AxiosRequestConfig = {
-	baseURL: 'https://api.harvestapp.com/api/v2/',
-	headers: {
-		'Content-Type': 'application/json',
-		'Harvest-Account-ID': `${processenv('HARVEST_ACCOUNT_ID')}`,
-		'User-Agent': 'Harvest-Mepco transformer',
-		'Authorization': `Bearer ${processenv('HARVEST_SECRET')}`,
-	}
-};
-const client: AxiosInstance = axios.create(config);
-
-client.interceptors.response.use((response: AxiosResponse) => {
-	// Any status code that lie within the range of 2xx cause this function to trigger
-	// Do something with response data
-	return response;
-}, (error: AxiosError) => {
-	// Any status codes that falls outside the range of 2xx cause this function to trigger
-	if (error.response) {
-		console.error(`Invalid response: ${error.response.status}, ${error.message} `);
-	} else if (error.request) {
-		console.error(`Request failed: ${error.request}`);
-	} else {
-		console.error(`Something failed: ${error.message}`);
-	}
-	return Promise.reject(error);
-});
+import { AxiosResponse } from 'axios';
+import {harvestClient} from './harvestClient';
 
 interface HarvestUser {
 	first_name: string;
@@ -38,7 +9,7 @@ interface HarvestUser {
 }
 
 async function getAllUsers(): Promise<HarvestUser[]> {
-	return client.get<HarvestUser[]>('/users')
+	return harvestClient.get<HarvestUser[]>('/users')
 	  .then((response: AxiosResponse) => {
 			//console.log(response.data);
 			return response.data.users;
@@ -46,7 +17,7 @@ async function getAllUsers(): Promise<HarvestUser[]> {
 }
 
 function findUser(name: string): void {
-	client.get('/users/me.json') // TODO Fix correct endpoint 
+	harvestClient.get('/users/me.json') // TODO Fix correct endpoint 
 		.then((response) => {
 			console.log(response.status);
 			console.log(response.data);
@@ -55,7 +26,7 @@ function findUser(name: string): void {
 
 function findUserById(id: number): Promise<HarvestUser> {
 	console.log(`Getting user ${id}`);
-	return client.get<HarvestUser>(`/users/${id}`)  
+	return harvestClient.get<HarvestUser>(`/users/${id}`)  
 		.then((response: AxiosResponse) => {
 			console.log(response.status);
 			//console.debug(response.data);
---
+++ integration/src/harvestClient.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { processenv } from 'processenv';
+
+const config: AxiosRequestConfig = {
+	baseURL: 'https://api.harvestapp.com/api/v2/',
+	headers: {
+		'Content-Type': 'application/json',
+		'Harvest-Account-ID': `${processenv('HARVEST_ACCOUNT_ID')}`,
+		'User-Agent': 'Harvest-integration',
+		'Authorization': `Bearer ${processenv('HARVEST_SECRET')}`,
+	}
+};
+
+export const harvestClient: AxiosInstance = axios.create(config);
+
+harvestClient.interceptors.response.use((response: AxiosResponse) => {
+	// Any status code that lie within the range of 2xx cause this function to trigger
+	// Do something with response data
+	return response;
+}, (error: AxiosError) => {
+	// Any status codes that falls outside the range of 2xx cause this function to trigger
+	if (error.response) {
+		console.error(`Invalid response: ${error.response.status}, ${error.message} `);
+	} else if (error.request) {
+		console.error(`Request failed: ${error.request}`);
+	} else {
+		console.error(`Something failed: ${error.message}`);
+	}
+	return Promise.reject(error);
+});
